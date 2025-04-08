9d192ad23846cef6c9ccaedb3516cdc7192d7696
jayden <jaydenmuller1907@gmail.com>
2025-04-08 19:17:37 +0200

    verbeteringen

--- packstore.js
+++ packstore.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-    var pack = document.getElementById("pokemon-pack");
+    var pack = document.getElementById("pack");
+    // alert("pack loaded");
     pack.addEventListener("click", openPack);
 }
 
@@ -9,34 +10,35 @@ function randomNumber(min, max) {
 
 function openPack() {
     // alert("open pack");
-    let cardsOpened = document.getElementById("pokemon-cards-opened");
+    let cardsOpened = document.getElementById("pakimon-cards-opened");
+    // alert(cardsOpened);
     while (cardsOpened.firstChild) {
         cardsOpened.firstChild.remove()
     }
 
     for (let i = 0; i < 11; i++) {
         /* 
-            <div class="pokemon-card"> 
+            <div class="pakimon-card"> 
                 <img id="0">
             </div>
         */
         let cardDiv = document.createElement("div");
-        cardDiv.classList.add("pokemon-card");
+        cardDiv.classList.add("pakimon-card");
 
         let cardImg = document.createElement("img");
         cardImg.id = i;
 
         let num = 1;
         if (i == 10) {
-            num = randomNumber(1, 16); //rare card for the last card
+            num = randomNumber(1, 4); //rare card for the last card
         } else {
-            num = randomNumber(17, 101); 
+            num = randomNumber(1, 4); 
         }
 
-        cardImg.src = "./pokemon-cards/base set (" + num.toString() + ").jpg";
+        cardImg.src = "/pakimon cards/base set (" + num.toString() + ").png";
 
         cardDiv.appendChild(cardImg);
 
-        document.getElementById("pokemon-cards-opened").appendChild(cardDiv);
+        document.getElementById("pakimon-cards-opened").appendChild(cardDiv);
     }
 }
